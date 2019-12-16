2b0339cf4753b7afb1777a71128d94da080684e4
Robin Ebert <ebert-robin@web.de>
2019-12-16 15:07:37 +0100

    Comments added to MathematicFunctions. Some logical mistakes fixed.

--- MathematicFunctions/HelperFunctions.cs
+++ MathematicFunctions/HelperFunctions.cs
@@ -1,19 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Molytho.TowerOfHanoi
+﻿namespace Molytho.TowerOfHanoi
 {
+    /// <summary>
+    /// Provides static functions needed to calculate the Tower-of-Hanoi-problems
+    /// </summary>
     public static class HelperFunctions
     {
+        /// <summary>
+        /// Calculate the potential of the given base and exponent.<br></br>
+        /// Only able to use positive integer values
+        /// </summary>
+        /// <param name="_base">The base of the potential function</param>
+        /// <param name="exponent">The exponent of potential function</param>
+        /// <returns>The solution of the potential function</returns>
         public static ulong Pow(ulong _base, ulong exponent)
         {
-            ulong ret = 1;
-            for(;exponent > 0; exponent--)
+            ulong ret = 1; //Set ret to 1 because of _base^0 = 1
+            for(; exponent > 0; exponent--) //Multiply ret exponent times with _base
             {
                 ret *= _base;
             }
-            return ret;
+            return ret; // return solution
+        }
+        
+        /// <summary>
+        /// Calculates the Binomial Coefficent
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="k"></param>
+        /// <returns>n nCr k</returns>
+        public static ulong BinomialCoefficent(ulong n, ulong k)
+        {
+            if(2 * k > n) //When k is over the symetrical axis of pascals triangle
+                k = n - k; //Then set k to its symetrical opponent
+            if(k == 0) //When k is 0
+                return 1; //Then the solution is always 1
+            ulong ergebnis = 1; //Set ergebnis to 1 because n nCr 0 = 1
+            for(ulong i = 1; i <= k; i++) //Multiply through the diagonal of pascals triangle
+            {
+                ergebnis = ergebnis * (n - k + i) / i; //because (n + 1) nCr (k + 1) = (n nCr k) * (n + 1) / (k + 1)
+            }
+            return ergebnis;
         }
     }
 }
--- MathematicFunctions/MathematicFunctions.cs
+++ MathematicFunctions/MathematicFunctions.cs
@@ -1,25 +1,20 @@
-﻿using System;
-
-namespace Molytho.TowerOfHanoi
+﻿namespace Molytho.TowerOfHanoi
 {
+    /// <summary>
+    /// Provides static functions for the Tower-of-Hanoi-MoveCount-problem
+    /// </summary>
     public static class MathematicFunctions
     {
-        public static ulong BinomialCoefficent(ulong n, ulong k)
-        {
-            if(k == 0)
-                return 1;
-            if(2 * k > n)
-                k = n - k;
-            ulong ergebnis = 1;
-            for(ulong i = 1; i <= k; i++)
-            {
-                ergebnis = ergebnis * (n - k + i) / i;
-            }
-            return ergebnis;
-        }
-
+        /// <summary>
+        /// Calculates the Increment of a <paramref name="pegCount"/>, <paramref name="diskCount"/> combination
+        /// </summary>
+        /// <param name="pegCount">Count of pegs</param>
+        /// <param name="diskCount">Count of disks</param>
+        /// <returns>Increment of the combination</returns>
         public static ulong Increment(ulong pegCount, ulong diskCount)
         {
+            if(pegCount < 3)
+                throw new System.ArgumentException("pegCounts smaller than 3 are senseless", "pegCount");
             if(diskCount == 0)
                 return 0;
             ulong increment = 0, temp = 1;
@@ -27,39 +22,53 @@ namespace Molytho.TowerOfHanoi
             while(diskCount > temp)
             {
                 increment++;
-                temp = (ulong)(temp * (pegCount - 2d + increment) / increment);
+                temp = temp * (pegCount - 2 + increment) / increment;
             }
 
             return increment + 1;
         }
-
+        /// <summary>
+        /// Calculates minimum moves needed for solving the Tower-of-Hanoi with 3+ pegs
+        /// </summary>
+        /// <param name="increment">The increment of the <paramref name="pegCount"/>, <paramref name="diskCount"/> combination</param>
+        /// <param name="pegCount">The peg count of the Tower-of-Hanoi game</param>
+        /// <param name="diskCount">The disk count of the Tower-of-Hanoi game</param>
+        /// <returns>The move count minimaly needed</returns>
         private static ulong MoveCountIntern(ulong increment, ulong pegCount, ulong diskCount)
         {
             ulong temp = diskCount * HelperFunctions.Pow(2, increment - 1);
 
             for(ulong i = 0; i < increment; i++)
             {
-                temp += HelperFunctions.Pow(2, i) * BinomialCoefficent(i + pegCount - 3, pegCount - 3);
+                temp += HelperFunctions.Pow(2, i) * HelperFunctions.BinomialCoefficent(i + pegCount - 3, pegCount - 3);
             }
             for(ulong i = 0; i < increment; i++)
             {
-                temp -= HelperFunctions.Pow(2, increment - 1) * BinomialCoefficent(i + pegCount - 3, pegCount - 3);
+                temp -= HelperFunctions.Pow(2, increment - 1) * HelperFunctions.BinomialCoefficent(i + pegCount - 3, pegCount - 3);
             }
 
             return temp;
-        }
+        } //Implementation for the formular
 
+        /// <summary>
+        /// Calculates minimum moves needed for solving the Tower-of-Hanoi with 3+ pegs
+        /// </summary>
+        /// <param name="pegCount">The peg count of the Tower-of-Hanoi game</param>
+        /// <param name="diskCount">The disk count of the Tower-of-Hanoi game</param>
+        /// <returns>The move count minimaly needed</returns>
         public static ulong MoveCount(ulong pegCount, ulong diskCount)
         {
-            if(pegCount == 3)
+            if(pegCount < 3) //Less than 3 pegs is nonsense
+                throw new System.ArgumentException("pegCounts smaller than 3 are senseless", "pegCount");
+            if(pegCount == 3) //3 pegs is simply 2^diskCount - 1
             {
                 return HelperFunctions.Pow(2, diskCount) - 1;
             }
 
             ulong increment;
 
-            increment = Increment(pegCount, diskCount);
-            return MoveCountIntern(increment, pegCount, diskCount);
+            increment = Increment(pegCount, diskCount); //Calculate the increment
+            return MoveCountIntern(increment, pegCount, diskCount); //and pass it to the intern function
         }
     }
 }

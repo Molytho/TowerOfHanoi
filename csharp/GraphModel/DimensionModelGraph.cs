553c04741eebe43b32b669b9a091954adfdf68e3
Robin Ebert <ebert-robin@web.de>
2020-02-10 13:23:50 +0100

    Added DimensionModelGraphProjection

--- csharp/GraphModel/DimensionModelGraph.cs
+++ csharp/GraphModel/DimensionModelGraph.cs
@@ -17,9 +17,9 @@ namespace Molytho.TowerOfHanoi
             Count = CalculateSize(pegCount, diskCount);
             _pointData = new T[Count];
 
-            _weights = new ushort[pegCount];
+            _weights = new ushort[diskCount];
             _weights[0] = 1;
-            for(ushort i = 1; i < pegCount; i++)
+            for(ushort i = 1; i < diskCount; i++)
                 _weights[i] = (ushort)(_weights[i - 1] * pegCount);
 
             Dimension = diskCount;
@@ -38,26 +38,24 @@ namespace Molytho.TowerOfHanoi
         {
             get
             {
-                int index = DimensionModelPoint.GetPointIndex(coords, _weights);
+                int index = GetPointIndex(coords);
                 return _pointData[index];
             }
             set
             {
-                int index = DimensionModelPoint.GetPointIndex(coords, _weights);
+                int index = GetPointIndex(coords);
                 _pointData[index] = value;
             }
         }
-    }
-    static class DimensionModelPoint
-    {
-        internal static int GetPointIndex(ushort[] coords, ushort[] weights)
+
+        internal int GetPointIndex(ushort[] coords)
         {
-            if(coords.Length != weights.Length)
+            if(coords.Length != _weights.Length)
                 throw new ArgumentException();
             int ret = 0;
             for(int i = 0; i < coords.Length; i++)
             {
-                ret += coords[i] * weights[i];
+                ret += coords[i] * _weights[i];
             }
             return ret;
         }
---
+++ csharp/GraphModel/DimensionModelGraphProjection.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Molytho.TowerOfHanoi
+{
+    public class DimensionModelGraphProjection<T>
+    {
+        private readonly DimensionModelGraph<T> _dimensionGraph;
+        private readonly DimensionModelGraphPointNeighbours _dimensionNeightbours;
+        public T this[ushort[] coords]
+        {
+            get
+            {
+                return _dimensionGraph[coords];
+            }
+            set
+            {
+                _dimensionGraph[coords] = value;
+            }
+        }
+        public List<ushort[]> GetNeighbours(ushort[] coords) => _dimensionNeightbours.GetNeighbours(coords);
+        public DimensionModelGraphProjection(ushort pegCount, ushort diskCount)
+        {
+            _dimensionGraph = new DimensionModelGraph<T>(pegCount, diskCount);
+            _dimensionNeightbours = new DimensionModelGraphPointNeighbours(_dimensionGraph.Count, _dimensionGraph.Lenght, _dimensionGraph.Dimension, _dimensionGraph.GetPointIndex);
+        }
+    }
+    class DimensionModelGraphPointNeighbours
+    {
+        private readonly ushort _lenght;
+        private readonly ushort _dimension;
+        public DimensionModelGraphPointNeighbours(int size, ushort lenght, ushort dimension, Func<ushort[],int> indexFunction)
+        {
+            _directionCache = new List<ushort>[size];
+            _lenght = lenght;
+            _dimension = dimension;
+            _indexFunction = indexFunction;
+        }
+        public List<ushort[]> GetNeighbours(ushort[] coords)
+        {
+            List<ushort[]> ret = new List<ushort[]>();
+            for(ushort i = 0; i < _dimension; i++)
+            {
+                if(!IsPointDimAllowed(i, coords))
+                    continue;
+                for(ushort l = 0; l < _lenght; l++)
+                {
+                    if(l == coords[i])
+                        continue;
+                    ushort[] coordsTemp = new ushort[_dimension];
+                    coords.CopyTo(coordsTemp, 0);
+
+                    coordsTemp[i] = l;
+                    if(IsPointDimAllowed(i, coordsTemp))
+                        ret.Add(coordsTemp);
+                }
+            }
+            return ret;
+        }
+
+        private readonly Func<ushort[], int> _indexFunction;
+        private readonly List<ushort>[] _directionCache;
+        private bool IsPointDimAllowed(ushort dimension, ushort[] coords)
+        {
+            int index = _indexFunction(coords);
+            _directionCache[index] ??= GetPointDimAllowed(coords);
+            List<ushort> pointDims = _directionCache[index];
+
+            return pointDims.Contains(dimension);
+        }
+        private List<ushort> GetPointDimAllowed(ushort[] coords)
+        {
+            List<ushort> ret = new List<ushort>();
+            for(ushort i = 0; i < _lenght; i++)
+            {
+                for(ushort index = 0; index < coords.Length; index++)
+                {
+                    if(coords[index] == i)
+                    {
+                        ret.Add(index);
+                        break;
+                    }
+                }
+            }
+            return ret;
+        }
+    }
+}

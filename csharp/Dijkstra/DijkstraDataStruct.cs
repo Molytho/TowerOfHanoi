d4a855631b1b8789192465bae99857d7c63b7219
Robin Ebert <ebert-robin@web.de>
2020-02-10 14:39:16 +0100

    Added DijkstraData struct

---
+++ csharp/Dijkstra/DijkstraDataStruct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Molytho.TowerOfHanoi
+{
+    struct DijkstraData
+    {
+        public DijkstraData(ushort[] previousPoint, uint depth)
+        {
+            PreviousPoint = previousPoint;
+            Depth = depth;
+        }
+        public ushort[] PreviousPoint { readonly get; set; }
+        public uint Depth { readonly get; set; }
+    }
+}

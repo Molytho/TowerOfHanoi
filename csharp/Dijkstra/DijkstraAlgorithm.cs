83f74995da6fa30a15a05f565b9126c2e4020b54
Robin Ebert <ebert-robin@web.de>
2020-02-10 19:27:44 +0100

    Fixed a bug where startPoint is not ignored in dijkstra

--- csharp/Dijkstra/DijkstraAlgorithm.cs
+++ csharp/Dijkstra/DijkstraAlgorithm.cs
@@ -23,17 +23,13 @@ namespace Molytho.TowerOfHanoi
         }
         public Task CalculateAsync(uint breakCondition)
         {
-
-            Graph[_startPoint] = new DijkstraData(null, uint.MaxValue); // Workaround that StartPoint is initialized
-            if(breakCondition == uint.MaxValue)
-                breakCondition = uint.MaxValue - 1;
-
-            return CalculateAsync(_startPoint, breakCondition, 0);
+            Graph[_startPoint] = new DijkstraData(new ushort[0], 0); //Workaround
+            return CalculateAsync(_startPoint, breakCondition, 1);
         }
 
         private async Task CalculateAsync(ushort[] point, uint breakCondition, uint depth)
         {
-            if(breakCondition == depth)
+            if(breakCondition < depth)
                 return;
 
             List<Task> tasks = new List<Task>();
@@ -43,7 +39,8 @@ namespace Molytho.TowerOfHanoi
             {
                 ushort[] neighbour = neighbourList[i];
                 uint neighbourDepth = Graph[neighbour].Depth;
-                if(neighbourDepth == 0 || neighbourDepth > depth)
+                var neighbourPoint = Graph[neighbour].PreviousPoint;
+                if(neighbourPoint is null || neighbourDepth > depth)
                 {
                     Graph[neighbour] = new DijkstraData(point, depth);
                     tasks.Add(CalculateAsync(neighbour, breakCondition, depth + 1));

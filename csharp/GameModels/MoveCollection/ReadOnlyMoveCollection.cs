2b26bc306ba3b216959aaf190f03eb6fa6910e92
Robin Ebert <ebert-robin@web.de>
2020-02-14 22:47:04 +0100

    ReadOnlyMoveCollection commented

--- csharp/GameModels/MoveCollection/ReadOnlyMoveCollection.cs
+++ csharp/GameModels/MoveCollection/ReadOnlyMoveCollection.cs
@@ -2,8 +2,16 @@
 
 namespace Molytho.TowerOfHanoi
 {
+    /// <summary>
+    /// Represents a readonly <see cref="MoveCollection"/>
+    /// </summary>
     public class ReadOnlyMoveCollection
     {
+        /// <summary>
+        /// Initializese a new instance of <see cref="ReadOnlyMoveCollection"/> class
+        /// </summary>
+        /// <param name="moves">Move array of <see cref="MoveCollection"/> object</param>
+        /// <param name="count">Count of objects in the <see cref="ReadOnlyMoveCollection"/> instance</param>
         internal ReadOnlyMoveCollection(Move[] moves, int count)
         {
             this.moves = moves;
@@ -13,6 +21,11 @@ namespace Molytho.TowerOfHanoi
         protected internal Move[] moves;
         protected internal int _count;
 
+        /// <summary>
+        /// Gets an Element of the <see cref="ReadOnlyMoveCollection"/>
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
         public Move this[int index]
         {
             get
@@ -24,6 +37,9 @@ namespace Molytho.TowerOfHanoi
             }
         }
 
+        /// <summary>
+        /// Gets the number of Elements in the <see cref="ReadOnlyMoveCollection"/>
+        /// </summary>
         public int Count => _count;
     }
 }

97c160fad15278d2442ca560764521719a3d7a4e
Robin Ebert <ebert-robin@web.de>
2020-02-14 21:56:20 +0100

    MathematicFunctions exceptions commented

--- csharp/MathematicFunctions/Exceptions.cs
+++ csharp/MathematicFunctions/Exceptions.cs
@@ -4,10 +4,17 @@ using System.Text;
 
 namespace Molytho.TowerOfHanoi
 {
+    /// <summary>
+    /// Exception indicating that there is not enough memory available for calculation
+    /// </summary>
     public class TooManyMovesException : Exception
     {
         public TooManyMovesException(ulong count) => MoveCount = count;
-        public ulong MoveCount { get; private set; }
+        public ulong MoveCount { get; private set; } //The count of move being needed
     }
+
+    /// <summary>
+    /// Exception indicating that the MoveCollectionSegment is full and isn't able to resize
+    /// </summary>
     public class OutOfArraySpaceException : Exception { }
 }

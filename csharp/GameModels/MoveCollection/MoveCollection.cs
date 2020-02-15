ea91b3724a273ae6a66c3174873c74a56ec0e0ab
Robin Ebert <ebert-robin@web.de>
2020-02-15 11:00:23 +0100

    MoveCollection commented

--- csharp/GameModels/MoveCollection/MoveCollection.cs
+++ csharp/GameModels/MoveCollection/MoveCollection.cs
@@ -2,23 +2,41 @@
 
 namespace Molytho.TowerOfHanoi
 {
+    /// <summary>
+    /// Represents the master class of a MoveCollection
+    /// </summary>
     public class MoveCollection : MoveCollectionBase
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MoveCollection"/> class with a capacity of 4
+        /// </summary>
         public MoveCollection() : this(4) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MoveCollection"/> class
+        /// </summary>
+        /// <param name="capacity">The capacity of the collection</param>
         public MoveCollection(int capacity) : base(capacity, 0)
         {
             moves = new Move[capacity];
         }
 
+        /// <summary>
+        /// Provide a way to resize the collection
+        /// </summary>
         protected override void Resize()
         {
-            _capacity *= 2;
-            Array.Resize(ref moves, _capacity);
+            _capacity *= 2; //Double the capacity
+            Array.Resize(ref moves, _capacity); //And resize the array
         }
+
+        /// <summary>
+        /// Pops the last element in the array
+        /// </summary>
+        /// <returns>The last element</returns>
         public Move RemoveLast()
         {
-            _count--;
-            return moves[_count + 1];
+            //Return the move and decrease the element count
+            return moves[_count--];
         }
     }
 }
